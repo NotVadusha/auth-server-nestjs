d1570205140645aa0df8ef06039a78e47437186e
Vadym <vadym.b@develops.today>
2024-07-08 23:33:25 +0300

    core: setup env and db into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ".env",
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.HOST,
+      port: Number(process.env.PORT || 3000),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+      synchronize: process.env.BUILD_TYPE === "development",
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
